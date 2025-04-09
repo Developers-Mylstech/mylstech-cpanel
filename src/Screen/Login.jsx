9a28bd5956f1170be0e0c1c6decae9064f02015b
User Yaseen <mohdyaseen7610@gmail.com>
2025-04-09 12:41:07 +0530

    chnages

--- src/Screen/Login.jsx
+++ src/Screen/Login.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Login() {
   return (
     <div>
-      Login
+      <div className='grid grid-cols-2 w-full h-full' >
+      <div className='relative'>
+        <div className='absolute inset-0 bg-black bg-opacity-40'></div>
+    <img className='h-screen w-full' src="https://img.freepik.com/free-vector/gradient-connection-background_23-2150462053.jpg?t=st=1744182115~exp=1744185715~hmac=a2a1aa037a4b1c323e09d9a0ab22ff416c05932ddaa2b37787a75a97bb638ec6&w=1380" alt="" />
+      </div>
+      <div>
+      <h1>login</h1>
+
+      </div>
+    </div>
     </div>
   )
 }
