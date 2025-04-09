e4327d05fa0c345d0c02c412feb66941101078f6
xyaistic <ziyamalik880@gmail.com>
2025-04-09 12:01:33 +0530

    Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pri: {
+          DEFAULT: '#2F80ED',
+        },
+        lightpri: '#EAF3FE',  
+        gray: {
+          DEFAULT: '#F0F0F0', 
+        },
+        lightgray: '#FEFEFE', 
+      },
+    },
   },
   plugins: [],
 }
-
