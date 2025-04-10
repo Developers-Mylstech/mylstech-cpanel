f8042cdd05888786e48fb38d52d684022baddebe
xyaistic <ziyamalik880@gmail.com>
2025-04-10 16:30:59 +0530

    Changes

--- src/Components/Widget/BottomNav.jsx
+++ src/Components/Widget/BottomNav.jsx
@@ -6,21 +6,20 @@ export default function BottomNav({ navItems }) {
         <>
             <div className='flex justify-center items-center  w-screen  fixed bottom-5 md:hidden'>
 
-                <div className="flex gap-10 relative justify-between bg-white border px-5 py-2 rounded-full shadow-md   mx-auto  ">
+                <div className="flex gap-5 relative justify-between bg-white border px-5 py-2 rounded-full shadow-md   mx-auto  ">
                     {navItems.map(({ label, path, icon }) => (
                         <NavLink
                         key={label}
                         to={path}
                         className={({ isActive }) =>
-                          `flex items-center justify-center bg-gray text-xs px-4 py-2 rounded-full text-center whitespace-nowrap overflow-hidden transition-all duration-500 ease-in-out ${
+                          `flex items-center justify-center  bg-gray text-base px-4 py-2 rounded-full text-center whitespace-nowrap overflow-hidden transition-all duration-500 ease-in-out ${
                             isActive
-                              ? 'text-black font-bold min-w-[7rem]' // replaces w-28
-                              : 'text-gray-400 min-w-[3.5rem]' // approximate collapsed width
+                              ? 'text-black font-bold min-w-20' // replaces w-28
+                              : 'text-gray-400 min-w-[3rem]' // approximate collapsed width
                           }`
                         }
                       >
-                            <span className="text-base">{icon}</span>
-                            <span className="hidden lg:inline ml-2 uppercase">{label}</span>
+                            <span className="text-lg">{icon}</span>
                         </NavLink>
                     ))}
                 </div>
